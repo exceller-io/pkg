20d5713d71843da479b9ca4fb532fafc14f47574
Ram Gopinathan <ramprakash.gopinathan@t-mobile.com>
2019-09-07 09:47:47 -0700

    cleanup

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 // =========================================================================
-// Copyright ©  2019 AppsByRam USA, Inc.
+// Copyright ©  2019 AppsByRam authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
